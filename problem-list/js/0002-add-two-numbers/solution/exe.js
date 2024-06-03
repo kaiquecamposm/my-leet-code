fb77464cdbc8d78f6815a8ddbb5cfa927a3558d4
Kaique <kaiquedecampos2@gmail.com>
2024-06-03 19:38:57 -0300

    wip: solving problem

--- problem-list/js/0002-add-two-numbers/solution/exe.js
+++ problem-list/js/0002-add-two-numbers/solution/exe.js
@@ -1,40 +1,58 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+class ListNode {
+  constructor(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 
-const l1 = [2,4,3], l2 = [5,6,4]
-// const l1 = [0], l2 = [0]
-// const l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+const l1 = new ListNode(2, new ListNode(4, new ListNode(3))), l2 = new ListNode(5, new ListNode(6, new ListNode(4)))
 
 var addTwoNumbers = function(l1, l2) {
-  console.log(l1)
-  console.log(l2)
-  console.log('--------------------')
+  let res;
+  let currentL1 = l1;
+  let currentL2 = l2;
 
-  const mapL1 = new Map()
-  const mapL2 = new Map()
+  while (currentL1 && currentL2) {
+    let sum = currentL1.val + currentL2.val
+    let restSum = 0;
 
-  for(let x = 0; x < l1.length; x++) {
-    mapL1.set(x, l1[x])
+    if (sum > 9) {
+      restSum = sum - 9
 
-    console.log(x, mapL1.get(x))
+      res = addLastNode(res, new ListNode(sum - 10))
+      continue;
+    }
+
+    res = addLastNode(res, new ListNode(sum + restSum))
+
+    currentL1 = currentL1.next;
+    currentL2 = currentL2.next;
   }
-  
-  for(let y = 0; y < l2.length; y++) {
-    mapL2.set(y, l2[y])
 
-    console.log(mapL2.values())
-  }
+  console.log(res)
+
+  return res
 };
+
+var addLastNode = function(currentNode, newNode) {
+  if (!currentNode) {
+    currentNode = newNode 
+    return currentNode
+  }
+
+  while (currentNode.next) {
+    currentNode = currentNode.next
+  }
+
+  currentNode.next = newNode
+
+  return currentNode
+}
 
 const res = addTwoNumbers(l1, l2)
 
